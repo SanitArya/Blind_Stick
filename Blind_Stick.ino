271aa397f440b8f28ec4458d12cf7dc6f174db80
Sanit Tech <67328643+SanitArya@users.noreply.github.com>
2023-11-14 13:56:12 +0530

    Update Blind_Stick.ino

--- Blind_Stick.ino
+++ Blind_Stick.ino
@@ -1,84 +1,109 @@
-#define TRIGGER_PIN1 2
-#define ECHO_PIN1 3
-#define TRIGGER_PIN2 4
-#define ECHO_PIN2 5
-#define TRIGGER_PIN3 6
-#define ECHO_PIN3 7
-#define TRIGGER_PIN4 8
-#define ECHO_PIN4 9
-#define BUZZER 10
-#define VIBRATOR 11
+int trig1 = 2;
+int echo1 = 3;
+int trig2 = 4;
+int echo2 = 5;
+int trig3 = 6;
+int echo3 = 7;
+int trig4 = 8;
+int echo4 = 9;
+int buzzer = 11;
+int vibrator = 12;
 
 long duration_1, duration_2, duration_3, duration_4;
 float distance_1, distance_2, distance_3, distance_4;
 
+int range = 50;
+int bottom_range = 21;
+
 void setup() {
   Serial.begin(9600);
 
-  pinMode(TRIGGER_PIN1, OUTPUT);
-  pinMode(TRIGGER_PIN2, OUTPUT);
-  pinMode(TRIGGER_PIN3, OUTPUT);
-  pinMode(TRIGGER_PIN4, OUTPUT);
+  pinMode(trig1, OUTPUT);
+  pinMode(trig2, OUTPUT);
+  pinMode(trig3, OUTPUT);
+  pinMode(trig4, OUTPUT);
 
-  pinMode(ECHO_PIN1, INPUT);
-  pinMode(ECHO_PIN2, INPUT);
-  pinMode(ECHO_PIN3, INPUT);
-  pinMode(ECHO_PIN4, INPUT);
+  pinMode(echo1, INPUT);
+  pinMode(echo2, INPUT);
+  pinMode(echo3, INPUT);
+  pinMode(echo4, INPUT);
 
-  pinMode(BUZZER, OUTPUT);
-  pinMode(VIBRATOR, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(vibrator, OUTPUT);
 
-  digitalWrite(BUZZER, LOW);
-  digitalWrite(VIBRATOR, LOW);
+  digitalWrite(buzzer, LOW);
+  digitalWrite(vibrator, LOW);
 
 }
 
 void loop() {
 
-  // Sensor No. 01
-  digitalWrite(TRIGGER_PIN1, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIGGER_PIN1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN1, LOW);
-  duration_1 = pulseIn(ECHO_PIN1, HIGH);
-  distance_1 = (duration_1 * 0.034) / 2;
+  distance_1 = Measure_Distance(trig1, echo1);
+  distance_2 = Measure_Distance(trig2, echo2);
+  distance_3 = Measure_Distance(trig3, echo3);
+  distance_4 = Measure_Distance(trig4, echo4);
 
-  // Sensor No. 02
-  digitalWrite(TRIGGER_PIN2, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIGGER_PIN2, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN2, LOW);
-  duration_2 = pulseIn(ECHO_PIN2, HIGH);
-  distance_2 = (duration_2 * 0.034) / 2;
+  Check_For_Distance();
+}
 
-  // Sensor No. 03
-  digitalWrite(TRIGGER_PIN3, LOW);
+int Measure_Distance(int trig, int echo)
+{
+  digitalWrite(trig, LOW);
   delayMicroseconds(2);
-  digitalWrite(TRIGGER_PIN3, HIGH);
+  digitalWrite(trig, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN3, LOW);
-  duration_3 = pulseIn(ECHO_PIN3, HIGH);
-  distance_3 = (duration_3 * 0.034) / 2;
+  digitalWrite(trig, LOW);
+  int duration = pulseIn(echo, HIGH);
+  int dis = (duration * 0.034) / 2;
+  return dis;
+}
 
-  // Sensor No. 04
-  digitalWrite(TRIGGER_PIN4, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIGGER_PIN4, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN4, LOW);
-  duration_4 = pulseIn(ECHO_PIN4, HIGH);
-  distance_4 = (duration_4 * 0.034) / 2;
-
-  if (distance_1 < 30 || distance_2 < 30 || distance_3 < 30 || distance_4 < 30)
+void Check_For_Distance()
+{
+  if (distance_1 < range)
   {
-    digitalWrite(BUZZER, HIGH);
-    digitalWrite(VIBRATOR, HIGH);
+    Produce_Continuous_Sound();
+    Serial.print("D1 \n");
+  }
+  else if (distance_2 < range)
+  {
+    Produce_Continuous_Sound();
+    Serial.print("D2 \n");
+  }
+  else if (distance_3 < range)
+  {
+    Produce_Continuous_Sound();
+    Serial.print("D3 \n");
+  }
+  else if (distance_4 > bottom_range)
+  {
+    Produce_Beep_Sound();
+    Serial.print("D4 \n");
   }
   else
   {
-    digitalWrite(BUZZER, LOW);
-    digitalWrite(VIBRATOR, LOW);
+    Stop_Sound();
   }
+}
+
+void Produce_Continuous_Sound()
+{
+  digitalWrite(buzzer, HIGH);
+  digitalWrite(vibrator, HIGH);
+}
+
+void Produce_Beep_Sound()
+{
+  digitalWrite(buzzer, HIGH);
+  digitalWrite(vibrator, HIGH);
+  delay(80);
+  digitalWrite(buzzer, LOW);
+  digitalWrite(vibrator, LOW);
+  delay(80);
+}
+
+void Stop_Sound()
+{
+  digitalWrite(buzzer, LOW);
+  digitalWrite(vibrator, LOW);
 }
